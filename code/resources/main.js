fe19a0c2d9f153cdef8e4517b96059404c020d4b
learnjs-demo <learnmyjs@hotmail.com>
2024-11-08 10:05:32 +0200

    Add v.1.0

---
+++ code/resources/main.js
@@ -0,0 +1,18 @@
+export const main = [
+    {
+        name: 'Кратко про курс',
+        article: ['./about.html'],
+    },
+    {
+        name: 'Взаимодействие с HTML',
+        test: ['./test.html?id=1','./test.html?id=2'],
+        practice: ['./practice.html?id=1'],
+        challenge: ['./challenge.html?id=1'],
+        materials: ['chapter_2.zip'],
+        video: ['https://youtu.be/6fNE-lkVxsY', 'https://youtu.be/hCcSQPMAgzw']
+    },
+    {
+        name: 'Список уроков',
+        article: ['./plan.html']
+    }
+];
